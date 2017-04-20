1b65b928ea50da7d051b29b57400a67dbbb95503
Patrick Burls <pburls@gmail.com>
2017-04-20 22:29:20 +0100

    add build script

---
+++ build.ps1
@@ -0,0 +1,20 @@
+﻿if (!$Env:env:APPVEYOR_BUILD_NUMBER ) {
+    $revision = '1'
+}
+else {
+    $revision = $Env:env:APPVEYOR_BUILD_NUMBER 
+}
+$revision = "ci-{0:D4}" -f [convert]::ToInt32($revision, 10)
+Write-Host "Running build as $revision"
+
+$project_file = '.\src\Ark3\Ark3.csproj'
+$test_file = '.\test\Ark3.Test\Ark3.Test.csproj'
+
+Write-Host "Restoring solution nuget dependencies"
+dotnet restore
+
+Write-Host "Running test project $test_file"
+dotnet test $test_file
+
+Write-Host "Packaging $project_file with version suffix $revision"
+dotnet pack $project_file --configuration Release --version-suffix=$revision
